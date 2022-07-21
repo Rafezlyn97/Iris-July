bc58d4e29504376c4016017618c91ca98e06448a
Rafezlyn97 <101541294+Rafezlyn97@users.noreply.github.com>
2022-07-21 15:26:01 +0800

    Update Iris.py

--- Iris.py
+++ Iris.py
@@ -28,8 +28,9 @@ st.subheader('User Input parameters')
 st.write(df)
 
 iris = pd.read_csv('https://raw.githubusercontent.com/Rafezlyn97/Iris-July/main/IRIS.csv')
-X = iris.data
-Y = iris.target
+X = iris.drop('species',axis = 1)
+
+Y = iris['species']
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
